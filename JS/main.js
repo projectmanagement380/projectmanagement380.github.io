c51ed5859ffde8317c8ca530c05daab53319e597
jaime <rosajaim86@gmail.com>
2018-06-08 21:48:55 -0600

    Updates

---
+++ JS/main.js
@@ -0,0 +1,42 @@
+
+let nav = document.getElementById('navigation');
+
+function toggleMenu() {
+    nav.classList.toggle('navigation--visible');
+}
+
+//description buttons
+function bookOneIntro() {
+    var x = document.getElementById("books_one_intro");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function bookTwoIntro() {
+    var x = document.getElementById("books_two_intro");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function bookThreeIntro() {
+    var x = document.getElementById("books_three_intro");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+// $(document).ready(function() {
+//     function setPaddingBottom() {
+//       $('.content').css('padding-bottom', $('footer').innerHeight());
+//     }
+//     setPaddingBottom();
+//     $(window).resize(setPaddingBottom);
+//   });
