73f51b4c9fc2baa15f4cf14316db083ec0922437
projectmanagement380 <prince.kristy0@gmail.com>
2018-06-11 11:15:31 -0600

    edits

--- JS/main.js
+++ JS/main.js
@@ -5,3 +5,37 @@ nav = document.getElementById('navigation');
 function toggleMenu() {
     nav.classList.toggle('navigation--visible');
 }
+
+
+let nav = document.getElementById('navigation');
+
+function toggleMenu() {
+   nav.classList.toggle('navigation--visible');
+}
+
+//description buttons
+function bookOneIntro() {
+  var x = document.getElementById("books_one_intro").style.display = "block";
+}
+
+function bookTwoIntro() {
+   var x = document.getElementById("books_two_intro");
+   if (x.style.display === "none") {
+       x.style.display = "block";
+   } else {
+       x.style.display = "none";
+   }
+}
+
+function bookThreeIntro() {
+   var x = document.getElementById("books_three_intro");
+   if (x.style.display === "none") {
+       x.style.display = "block";
+   } else {
+       x.style.display = "none";
+   }
+}
+
+
+
+
