ccff2140d2578c2a3b10122ad9d88544b1038f94
jaime <rosajaim86@gmail.com>
2018-07-08 21:29:44 -0600

    fixing picture error 404

--- JS/main.js
+++ JS/main.js
@@ -46,7 +46,6 @@ function openNav() {
     document.getElementById("main").style.display = "none";
     document.getElementById("mySidenav").style.width = "251.10px";
     document.getElementById("main").style.marginLeft = "250px";
-    // document.getElementById("rotate").style.transform.rotate = "0.5turn";
 
 
 }
@@ -55,28 +54,26 @@ function closeNav() {
     document.getElementById("main").style.display = "block";
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
-  
+
 }
 
-// function rotateArrow() {
-//    var rotate = document.getElementById("rotate").style.transform.rotate = "0.5turn";
-//    if(rotate)
-// }
+// $(function() {
+//     $("#toggle").on('click', function() {   
+//        $(".fa-caret-down").toggleClass("rotate");
+//     });
+// });
 
 var dropdown = document.getElementsByClassName("dropbtn");
 var i;
 
 for (i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var dropdownContent = this.nextElementSibling;
-    if (dropdownContent.style.display === "block") {
-      dropdownContent.style.display = "none";
-    } else {
-      dropdownContent.style.display = "block";
-    }
-  });
+    dropdown[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === "block") {
+            dropdownContent.style.display = "none";
+        } else {
+            dropdownContent.style.display = "block";
+        }
+    });
 }
-
-    
-
